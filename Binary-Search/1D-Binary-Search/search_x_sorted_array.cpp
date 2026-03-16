ddad6d3e4fae374f55fdcffc1058829c5c65d1bf
HarshitZenhit3 <166893997+harshitkr13@users.noreply.github.com>
2026-03-16 18:31:17 +0530

    Create search_x_sorted_array.cpp

---
+++ Binary-Search/1D-Binary-Search/search_x_sorted_array.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int>& arr, int target) {
+    int low = 0, high = arr.size() - 1;
+
+    while(low <= high){
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == target) return mid;
+        else if(arr[mid] < target) low = mid + 1;
+        else high = mid - 1;
+    }
+
+    return -1;
+}
+
+int main() {
+    vector<int> arr = {1,3,5,7,9};
+    int target = 7;
+
+    cout << binarySearch(arr,target);
+}
