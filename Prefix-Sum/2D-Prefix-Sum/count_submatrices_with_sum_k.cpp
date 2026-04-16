788359e81b5308cf8712eac67c73d7b0e3ff42a6
HarshitZenhit3 <166893997+harshitkr13@users.noreply.github.com>
2026-04-16 11:44:00 +0530

    Implement 2D prefix sum for submatrix sum count

---
+++ Prefix-Sum/2D-Prefix-Sum/count_submatrices_with_sum_k.cpp
@@ -0,0 +1,34 @@
+//Number of Submatrices That Sum to Target (LC-1074)
+
+class Solution {
+public:
+    int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+
+        for(int i = 0; i < n; i++)
+            for(int j = 1; j < m; j++)
+                matrix[i][j] += matrix[i][j - 1];
+
+        int count = 0;
+
+        for(int c1 = 0; c1 < m; c1++) {
+            for(int c2 = c1; c2 < m; c2++) {
+                unordered_map<int, int> mp;
+                mp[0] = 1;
+
+                int sum = 0;
+
+                for(int r = 0; r < n; r++) {
+                    int val = matrix[r][c2] - (c1 > 0 ? matrix[r][c1 - 1] : 0);
+                    sum += val;
+
+                    if(mp.find(sum - target) != mp.end()) count += mp[sum - target];
+
+                    mp[sum]++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
